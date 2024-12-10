170b9c87a47974fbacc51d9a8cc7d02e56d441b7
alvesxy <alvescauaferreira@icloud.com>
2024-12-10 02:16:07 -0300

    Code to read chip
    Configuration of the MFRC522 to read the value of a chip and convert to decimal.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,35 @@
+#include <MFRC522.h>
+#include <SPI.h>
+
+#include <ReadFile.h>
 #include <Arduino.h>
 
-void setup () {
+MFRC522 identificator;
 
+void setup() {
+
+    Serial.begin(115200);
+    SPI.begin();
+
+    ReadFile read = ReadFile("/application.json");
+    
+    identificator = MFRC522(read.getSS(), read.getRST());
+    identificator.PCD_Init();
+    
 }
 
-void loop () {
-  
+void loop() {
+
+    if (!identificator.PICC_IsNewCardPresent() || !identificator.PICC_ReadCardSerial()) {
+        return;
+    }
+
+    String identifier;
+
+    for (byte i = 0; i < identificator.uid.size; i++) {
+        identifier += identificator.uid.uidByte[i], DEC;
+    }
+
+    identificator.PICC_HaltA();
+
 }
