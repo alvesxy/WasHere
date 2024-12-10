86be817dd4bc2078138f09f5cf68dcec8dcf0349
alvesxy <alvescauaferreira@icloud.com>
2024-12-10 00:47:25 -0300

    ReadFile.h created with a constructor and attribute
    The constructor read a file and keep in the attribute content.

---
+++ src/ReadFile.h
@@ -0,0 +1,39 @@
+#ifndef READFILE_H
+#define READFILE_H
+
+#include <Arduino.h>
+
+#include <FS.h>
+#include <LittleFS.h>
+
+class ReadFile {
+
+    private:
+
+        String content;
+
+    public:
+
+        /**
+         * Read a file with Littlefs
+         * @param local is the directory until the file
+        */
+        ReadFile (String local) {
+            
+            if (!LittleFS.begin()) Serial.println("Failed to start the LittleFS in ReadJson.h");
+                
+            File file = LittleFS.open(local, "r");
+
+            String content;
+
+            while (file.available()) {
+                content += (char)file.read();
+            }
+        
+            file.close();
+
+            this->content = content;
+        }
+};
+
+#endif
